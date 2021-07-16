9f95738d2ee63f60c94dbe9105715cb07f70e25e
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-07-16 18:24:14 +0700

    Update footer.inc.php

--- theme/axcora/footer.inc.php
+++ theme/axcora/footer.inc.php
@@ -42,7 +42,7 @@
 <footer class="clearfix">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 	<?php get_footer(); ?>
-	<div class="wrapper text-center p-1 col-12">support by <a href="https://sourceforge.net/p/vpos/"><img alt="Download vpos" src="https://sourceforge.net/sflogo.php?type=13&group_id=3374596" ></a><br/> <small><small>copyrights <?php echo date('Y'); ?> <a class="text-dark" href="<?php get_site_url(); ?>" ><?php get_site_name(); ?></a> - by <a class="text-dark" href="https://axcora.com/getaxcoracms/" target="_blank">get axcora cms</a></small></small>
+	<div class="wrapper text-center p-1 col-12"><small><small>copyrights <?php echo date('Y'); ?> <a class="text-dark" href="<?php get_site_url(); ?>" ><?php get_site_name(); ?></a> - by <a class="text-dark" href="https://axcora.com/getaxcoracms/" target="_blank">get axcora cms</a></small></small>
 	</div>
 	</div>
 	</div>
